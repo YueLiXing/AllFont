0e1e763cfad4e587fe5255c267731fe044dc86e9
yuelixing <1223343444@qq.com>
2023-01-02 19:33:00 +0800

    添加 ignore

---
+++ All-Font/CustomViewController.h
@@ -0,0 +1,17 @@
+//
+//  CustomViewController.h
+//  All-Font
+//
+//  Created by yuelixing on 2023/1/2.
+//  Copyright © 2023 YueLiXing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CustomViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ All-Font/CustomViewController.m
@@ -0,0 +1,32 @@
+//
+//  CustomViewController.m
+//  All-Font
+//
+//  Created by yuelixing on 2023/1/2.
+//  Copyright © 2023 YueLiXing. All rights reserved.
+//
+
+#import "CustomViewController.h"
+
+@interface CustomViewController ()
+
+@end
+
+@implementation CustomViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
